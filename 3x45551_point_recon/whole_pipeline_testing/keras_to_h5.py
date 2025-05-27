01c5f7354d90709efdaed1d5cffd5cf51a720897
RufusN <rufus@neame.org>
2025-05-27 16:52:24 +0100

    kept only bare minimum to work on essential scripts

--- 3x45551_point_recon/whole_pipeline_testing/keras_to_h5.py
+++
@@ -1,7 +0,0 @@
-from tensorflow import keras
-
-# 1. Load your .keras model
-model = keras.models.load_model('3x45551_950_1050.keras')
-
-# 2. Re-save it in HDF5 format
-model.save('3x45551_950_1050.h5', save_format='h5')
